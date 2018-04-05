1b6ec9ac207de42ae41a5dc0530e37ac8645be55
doublegames-hd <koekiemonster30@outlook.com>
2018-04-05 22:57:33 +0200

    Create nick.js

---
+++ commands/nick.js
@@ -0,0 +1,20 @@
+
+module.exports.run = async (client, message, arg) => {
+
+  const Discord = require('discord.js')
+  const webhooksend = require("quick.hook")
+  let infoText = arg.slice(1).join(" ");
+  message.guild.members.get(bot.user.id).setNickname(alertText);
+  const embed = new Discord.RichEmbed()
+  .setTitle("Info")
+  .setColor("#0284ff")
+  .setDescription("Nickname changed to: " + infoText)
+  .setFooter("© 2018 Electry Development | All Rights Reserved")
+  .setTimestamp()
+message.channel.send("Message created!")
+message.guild.channels.get(config.infoChannelID).send({embed});
+}
+
+module.exports.config = {
+  command: "nick"
+}
